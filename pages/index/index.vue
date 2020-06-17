a87dd16007357ea8e3e542043035066d26d66207
zhangjing <zhangjing@huixiangtech.cn>
2020-06-17 18:33:32 +0800

    1，提交代码

---
+++ pages/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view>
+		<screenShot @getSreenShot="getSreenShot">
+			<image src="/static/banner.jpg" mode="" :style="{
+                        height: windowHeight + 'px',
+                        width: windowWidth + 'px'
+                    }"></image>
+		</screenShot>
+	</view>
+</template>
+
+<script>
+	import screenShot from '@/components/screen-shot/screen-shot.vue';
+	export default {
+		components: {
+			screenShot
+		},
+		data() {
+			return {
+				windowWidth: '',
+				windowHeight: ''
+			};
+		},
+		onLoad() {
+			let _this = this;
+			uni.getSystemInfo({
+				success: function(res) {
+					_this.windowHeight = res.windowHeight;
+					_this.windowWidth = res.windowWidth;
+				},
+				fail: function(err) {
+					console.log(err);
+				}
+			});
+		},
+		methods: {
+			getSreenShot(r) {
+				// 获取截图路径
+				console.log('r', r);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
